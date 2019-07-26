5a1b7d29cc669537521a6112894f578297dc66ed
Gabriel Zavando <gabrielzavando@gmail.com>
2019-07-26 19:34:57 -0400

    Termine la primera version del sitio

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,6 @@
-import {mq768} from './lib/var-basicas'
 import {createModal} from './lib/elements'
-import {mpC, mpG, mpImg} from './assets/var-mapa'
 import {bMForm, bMOHistoria, mHistoria, bMCHistoria} from './assets/var-actions'
-import {Form} from './assets/var-modal';
-import {Mapa} from './dev/mapa'
+import {Form} from './assets/var-modal'
 import {toggleNav, scrollNav, animaHeader, colorHeader, showButtonUp, upToTop} from './dev/nav'
 import {CopyDate} from './dev/footer'
 import {printModal, printModalClasic} from './dev/modal'
@@ -18,4 +15,3 @@ showButtonUp()
 upToTop()
 printModal(bMForm, createModal,Form)
 printModalClasic(bMOHistoria, mHistoria, bMCHistoria)
-Mapa(mq768, mpC, mpG, mpImg)
